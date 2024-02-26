2d111b125629adcc84b553450eccfa43cc462f27
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-02-26 22:35:12 +0500

    updated

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
-    <div className='min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
-      <div className=''>
-
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Login to your account</h2>
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
