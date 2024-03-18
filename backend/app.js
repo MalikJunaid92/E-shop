6f0a8ce5244ff026637cdff3f1bba944bb0cd93b
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-03-19 01:08:45 +0500

    setup backend

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express= require('express')
+
+
+
+
+const app=express()
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+      path: "config/.env",
+    });
+  }
+module.exports= app;
