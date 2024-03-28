856dbc13ffbf80ed761d3753acd9314c8b64b1d0
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-03-29 03:27:01 +0500

    updated activation page

--- frontend/src/pages/ActivationPage.jsx
+++ frontend/src/pages/ActivationPage.jsx
@@ -1,3 +1,5 @@
+// ActivationPage.jsx
+
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
@@ -6,38 +8,41 @@ import { server } from "../server";
 
 const ActivationPage = () => {
   const { activation_token } = useParams();
-  const [error, setError] = React.useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     if (activation_token) {
       const activationEmail = async () => {
         try {
-          const res = await axios.post(`${server}/activation`, {
-            activation_token,
+          const res = await axios.get(`${server}/user/activation`, {
+            params: { activation_token }, // Send activation token as a query parameter
           });
           console.log(res.data.message);
         } catch (error) {
-          console.log(error.responce.data.message);
+          console.log(error.response.data.message);
+          setError(true);
         }
       };
       activationEmail();
     }
   }, [activation_token]);
-  return(
+
+  return (
     <div
-    style={{
-      width: "100%",
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    {error ? (
-      <p>Your token is expired!</p>
-    ) : (
-      <p>Your account has been created suceessfully!</p>
-    )}
-  </div>
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {error ? (
+        <p>Your token is expired!</p>
+      ) : (
+        <p>Your account has been created successfully!</p>
+      )}
+    </div>
   );
 };
 
