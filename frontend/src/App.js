3ec765554b152ee75a7d1723a0a53b8526af2432
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-03-31 02:33:16 +0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import { Login, SignupPage, ActivationPage } from "./Routes";
+import axios from "axios";
+import { server } from "./server";
+import { toast } from "react-toastify";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(`${server}/user/getuser`, { withCredentials: true })
+      .then((res) => {
+        toast.success(res.data.message);
+      }).catch((err)=>{
+        toast.error(err.response.data.message)
+      })
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,11 +1,40 @@
-import React, { useState } from "react";
+import React , { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
-import {Link} from 'react-router-dom'
-function Login() {
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
+
+const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post(
+        `${server}/user/login-user`,
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        toast.success("Login Success!");
+        navigate("/");
+        // window.location.reload(true); 
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+        
+      });
+  };
+
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -15,7 +44,7 @@ function Login() {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="email"
@@ -101,13 +130,15 @@ function Login() {
             </div>
             <div className={`${styles.noramlFlex} w-full`}>
               <h4>Not have any account?</h4>
-              <Link to='/sign-up' className='text-blue-600 pl-2'>Sign Up</Link>
+              <Link to="/sign-up" className="text-blue-600 pl-2">
+                Sign Up
+              </Link>
             </div>
           </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
