cf371c0b66c3b6fc3f089ba785de3b38e627b724
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-03-31 02:52:18 +0500

    set react redux

---
+++ frontend/src/redux/actions/user.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+export const loadUser = () => async (dispatch) => {
+    try {
+      dispatch({
+        type: "LoadUserRequest",
+      });
+      const { data } = await axios.get(`${server}/user/getuser`, {
+        withCredentials: true,
+      });
+      dispatch({
+        type: "LoadUserSuccess",
+        payload: data.user,
+      });
+    } catch (error) {
+      dispatch({
+        type: "LoadUserFail",
+        payload: error.response.data.message,
+      });
+    }
+  };
