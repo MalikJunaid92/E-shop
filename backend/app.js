a5410a31ca5b3b687f070c58808a0d8d0a44ff4c
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-07-13 12:52:53 +0500

    backend complete update

--- backend/app.js
+++ backend/app.js
@@ -1,51 +1,3 @@
-// const express = require("express");
-// const ErrorHandler = require("./middleware/error");
-// const app = express();
-// const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
-// const user = require("./controller/user");
-// const shop = require("./controller/shop");
-// const product = require("./controller/product");
-// const event = require("./controller/event");
-// const coupon = require("./controller/coupounCode");
-// const payment = require("./controller/payment");
-// const order = require("./controller/order");
-// const cors = require("cors");
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
-// app.use("/", express.static("uploads"));
-// app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-
-// // config
-// if (process.env.NODE_ENV !== "PRODUCTION") {
-//   require("dotenv").config({
-//     path: "config/.env",
-//   });
-// }
-// // import routes
-
-// app.use("/api/v2/user", user);
-// app.use("/api/v2/shop", shop);
-// app.use("/api/v2/product", product);
-// app.use("/api/v2/event", event);
-// app.use("/api/v2/coupon", coupon);
-// app.use("/api/v2/payment", payment);
-// app.use("/api/v2/order", order);
-
-
-// // its for Error Handler
-// app.use(ErrorHandler);
-// module.exports = app;
-
-
-
 const express = require("express");
 const ErrorHandler = require("./middleware/error");
 const app = express();
@@ -53,21 +5,18 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: ['http://localhost:5000 ',],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
-app.use(bodyParser.json({ limit: '100mb' }));
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
